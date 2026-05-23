e14f3d1dc1e96fcb0ba5e67c499d1a5c138c97bf
itschargeoclock <162012077+itschargeoclock@users.noreply.github.com>
2026-05-24 06:02:45 +1000

    feat: add interface.cpp for ESP32-DIV V2

---
+++ boards/esp32-div-v2/interface.cpp
@@ -0,0 +1,25 @@
+#include <interface.h>
+
+void _setup_gpio() {
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+    
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, LOW);
+}
+
+int getBattery() {
+    return 100;
+}
+
+bool isCharging() { return false; }
+
+void _setBrightness(uint8_t brightval) {
+    analogWrite(TFT_BL, (brightval * 255) / 100);
+}
+
+void InputHandler(void) {
+}
+
+void powerOff() {}
+void checkReboot() {}
