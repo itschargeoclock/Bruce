ace31edb979f9d4acef0279ff196caeddee931bc
itschargeoclock <162012077+itschargeoclock@users.noreply.github.com>
2026-05-26 21:34:48 +1000

    restore: restore 5 truncated core files from main branch

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -3,6 +3,16 @@
 #include "core/wifi/wifi_common.h"
 #include "current_year.h"
 #include "display.h"
-#kif !defined(LITE_VERSION) && !defined(DISABLE_INTERPRETER)
+#if !defined(LITE_VERSION) && !defined(DISABLE_INTERPRETER)
 #include "modules/bjs_interpreter/interpreter.h"
 #endif
+#include "modules/ble_api/ble_api.hpp"
+#include "modules/others/qrcode_menu.h"
+#include "modules/rf/rf_utils.h" // for initRfModule
+#include "mykeyboard.h"
+#include "powerSave.h"
+#include "sd_functions.h"
+#include "settingsColor.h"
+#include "utils.h"
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <globals.h>
